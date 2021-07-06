7ba8b4f0d9f9dc5c8a36d66beafc58c9950a5c1a
Antonio Almeida <promatik@gmail.com>
2021-07-06 14:02:40 +0100

    Added all pages

---
+++ 2022/index.js
@@ -0,0 +1,15 @@
+import { onDomReady } from 'cantil';
+
+const app = () => {
+  // Init Carousel
+  queryAll('.carousel .row img').forEach(
+    image => (image.onclick = () => {
+      query('.carousel .container img').src = image.src;
+
+      image.closest('.row').query('.active').classList.remove('active');
+      image.closest('.column').classList.add('active');
+    })
+  );
+};
+
+onDomReady().then(app);
